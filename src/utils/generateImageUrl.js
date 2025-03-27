0914d7e2113563d7c246a6fab8792eb3d75d484b
TRI-HM-PC-HOME <huynhminhtri.official@gmail.com>
2025-03-27 19:47:30 +0700

    fix: generate image url

--- src/utils/generateImageUrl.js
+++ src/utils/generateImageUrl.js
@@ -1,14 +1,23 @@
+import { convertNumberToStringWithLength } from "./convertNumberToSpecifiedLength.js";
+
 const numberCharacterLength = 5; // Number of characters in the image name
-function generateImageUrl(prefixPath, start, end, version = 1) {
+
+/**
+ * Generates an array of image URLs based on a prefix path, start and end numbers, and an optional version.
+ *
+ * @param {string} prefixPath - The base path for the images.
+ * @param {number} start - The starting number for the image sequence.
+ * @param {number} end - The ending number for the image sequence.
+ * @param {number} [version=1] - The version number to append to the image URLs for cache busting.
+ * @returns {string[]} An array of generated image URLs.
+ */
+export function generateImageUrl(prefixPath, start, end, version = 1) {
   const urls = [];
   for (let i = start; i <= end; i++) {
-    urls.push(`${prefixPath}${convertNumberToStrWithLen(
+    urls.push(`${prefixPath}${convertNumberToStringWithLength(
       i,
-      NUMBER_CHARACTER_LENGTH
+      numberCharacterLength
     )}.webp?v=${version}`);
   }
   return urls;
 }
-const result = generateImageUrl("/images/n2/", 0, 30, 1)
-
-console.log(result);
